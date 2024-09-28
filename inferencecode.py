01f2f914b6c35fdbf4751990cb55a779bb5c48e6
Jelal Sultanov <jelaleddinsultanov@yandex.ru>
2024-09-28 14:24:15 +0300

    refactored inferencecode.py

--- inferencecode.py
+++ inferencecode.py
@@ -1,52 +1,25 @@
-    # url = "http://localhost:8000/feedback"
-    # payload = {"image_path": image_path,
-    #                  "classification_decision": result}
-    # response = requests.post(url, json=payload)
-    # feedback = json.loads(response.content.decode('utf-8'))
-    # operator_feedback = feedback["feedback"]
-"""
-This script contains functions for making inferences on images using
-a pre-trained model for NSFW (Not Safe For Work) content detection.
-It integrates with FastAPI to serve inference requests through HTTP endpoints.
-
-Functions:
-1. download_file(url: str) -> bytes:
-    Downloads a file from the specified URL and returns its content as bytes.
-
-2. decision_function(result, image_path: str) -> JSONResponse:
-    Analyzes the classification result and decides whether an image contains NSFW content or not.
-    Writes analytics to files and returns a JSON response.
-
-3. make_inference(url: str = Form(None),
-                            file: UploadFile = File(None),
-                            image_path: str = Form(None)) -> JSONResponse:
-    Handles inference requests by downloading the image file from a URL, uploading a file,
-    or specifying a local file path. Performs NSFW content detection and returns a JSON response.
-
-"""
-from datetime import datetime
-from fastapi import File, Form, UploadFile
-from fastapi.responses import JSONResponse
-import json
-import requests
-from nsfw_detector import predict
-from ml_data_utils import load_images_from_bytes, load_image_from_url, download_file
+import os
+import io
 import traceback
-import requests
 import yaml
+from datetime import datetime
+from fastapi.responses import JSONResponse
+from nsfw_detector import predict
+from ml_data_utils import load_images_from_bytes, download_file
 
+# Load constants
 def load_constants(file_path):
     with open(file_path, 'r') as file:
         constants = yaml.safe_load(file)
     return constants
+
 constants = load_constants('constants.yaml')
 returning = constants["RETURN_MESSAGES"]
-#NUMBER_OF_MISCLASSIFICATIONS = 0
 IMAGE_DIM = constants['IMAGE_DIM']
-# Load the model when the application starts
 MODEL_PATH = constants['MODEL_PATH']
-model = predict.load_model(MODEL_PATH)
 
+# Load the model when the application starts
+model = predict.load_model(MODEL_PATH)
 
 def decision_function(result, image_path):
     """
@@ -57,9 +30,8 @@ def decision_function(result, image_path):
         image_path (str): The path of the image.
 
     Returns:
-        JSONResponse: The JSON response containing the decision.
+        dict: The dictionary containing the decision.
     """
-    #global NUMBER_OF_MISCLASSIFICATIONS
     try:
         nsfw_threshold = 0.95
         nsfw_categories = ["snail", "slug"]
@@ -71,38 +43,33 @@ def decision_function(result, image_path):
             else:
                 data = {"decision": False, "detailed_info": max_probs}
 
-            return JSONResponse(content={"status": data["decision"],
-                                        "message": "СКАЙНЕТ РАБОТАЕТ",
-                                        "code": "SS-10000", "data": data,
-                                        "image_path": image_path})
-
+            return {
+                "status": data["decision"],
+                "message": "СКАЙНЕТ РАБОТАЕТ",
+                "code": "SS-10000",
+                "data": data,
+                "image_path": image_path
+            }
     except Exception as e:
         traceback.print_exc()  # Print the traceback for debugging
-        return JSONResponse(content={"Message": str(e)})
+        return {"Message": str(e)}
 
-def make_inference(file: UploadFile = File(None),
-                            image_path: str = Form(None)):
+def make_inference(image_path: str, image_file: bytes):
     """
     Makes inference on the provided image data.
 
     Args:
-        url (str, optional): URL of the image. Defaults to None.
-        file (UploadFile, optional): Uploaded image file. Defaults to None.
-        image_path (str, optional): Path of the image. Defaults to None.
+        image_data (bytes): Image data in bytes.
+        image_path (str): Optional parameter to specify the image path.
 
     Returns:
-        JSONResponse: The JSON response containing the inference result.
-
-    Raises:
-        ValueError: If URL, file, or image_path was not provided.
-
+        dict: The dictionary containing the inference result.
     """
-
     try:
-        downloaded_image, downloaded_path = download_file(file, image_path)
-        nd_image = load_images_from_bytes(downloaded_image)
+        # Load image and make inference
+        nd_image = load_images_from_bytes(image_file)
         result = predict.classify_nd(model, nd_image)
-        return decision_function(result, downloaded_path)
+        return decision_function(result, image_path)
     except Exception as e:
         global returning
         returning["status"] = False
@@ -110,4 +77,3 @@ def make_inference(file: UploadFile = File(None),
         returning["code"] = "SS-10000"
         returning["data"] = str(e)
         return returning
-#СКАЙНЕТ РАБОТАЕТ
